6d144adabe9539df2dff3dbb9d83f95548625dc4
Eugen <epopescuu@yahoo.it>
2023-11-03 15:52:25 +0100

    refactor: add while

--- main.js
+++ main.js
@@ -12,32 +12,57 @@ const imagesPath = [
 let imagesAdd = "";
 
 // ciclo le immagini (imagesPath)
-for (let i = 0; i < imagesPath.length; i++) {
-    // salvo ogni percorso
-    const imagePath = imagesPath[i];    
+// for (let i = 0; i < imagesPath.length; i++) {
+//    //  salvo ogni percorso
+//     const imagePath = imagesPath[i];    
 
+//     // inserisco nella stringa vuota il codice html del singolo percorso all'atributo src 
+//     imagesAdd += `<img src="${imagePath}">`;
+// }
+
+// CICLO WHILE
+// specifichiamo solo la condizione da dicchiarare ad ogni giro
+// dicchiaro la variabile fuori
+let i = 0;
+while (i < imagesPath.length) {
+    const imagePath = imagesPath[i]; 
     // inserisco nella stringa vuota il codice html del singolo percorso all'atributo src 
     imagesAdd += `<img src="${imagePath}">`;
-}
+
+    // si prende la stringa popolata e si stampa nel DOM il codice con l'immagine
+    document.querySelector('.carousel-container').innerHTML += imagesAdd;
+
+    // incremento i di uno a fine di ogjni giro
+    i++;
+} 
 // si prende la stringa popolata e si stampa nel DOM il codice con l'immagine
-document.querySelector('.carousel-container').innerHTML += imagesAdd;
+// document.querySelector('.carousel-container').innerHTML += imagesAdd;
 
 // definisco una variabile, e si assegna l'index 0, impostando la prima immagine come attiva 
 let currentImage = [0];     
 // seleziono tutte le immagini attraverso il querySelectorAll che pesca i tag di tutte le immagini presenti nel container
+// mi restituisce un array di immagini, e con currentImage prendo l'immagine dell'array e la imposto a 0
 const images = document.querySelectorAll('.carousel-container img');
 // do la classe active (display: block) alla prima immagine dell'array, assegnando display: non a tute le altre
 images[currentImage].classList.add('active');
 
-// BOTTON UP 
+
+
+// NEXT BUTTON 
 document.querySelector('#nextBtn').addEventListener('click', function() {
     console.log('avanti');
 
-    // 
+    // se lelemento ha n elementi, l'ultimo elemento sarà n-1
+    // ci interessa andare avanti fino a quando non raggiungo l'ultima immagine
     if (currentImage < images.length-1) {
+        
+        // rimuovo la clsse active(display:block) e rendo l'immagine corrente invisibile
         images[currentImage].classList.remove('active');
+        // aggiorno il contatore dell'immagine attiale, incrementandolo di 1
         currentImage++;
+        // aggiungo la visibilità sull'immagine incrementata
         images[currentImage].classList.add('active');
+
     } else if (currentImage == images.length-1) {
         images[currentImage].classList.remove('active');
         currentImage = [0];
