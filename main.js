c9355553e94b2038dd5317f5dadbf8fd535ab802
Eugen <epopescuu@yahoo.it>
2023-11-03 01:23:05 +0100

    refactor(main.js): add an array with img path

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
-// do la posizione iniziale
-let currentImage = 0;
+// Perscorso immagini
+//Si crea un array contenente i percorsi delle foto, in questo caso non sono link"
+const imagesPath = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp"
+]
 
-// seleziono tutte le immagini attraverso il querySelectorAll
+// creo una stringa vuota per inserire le immagini nel DOM a ciclo finito
+let imagesAdd = "";
+
+// ciclo le immagini (imagesPath)
+for (let i = 0; i < imagesPath.length; i++) {
+    // salvo ogni percorso
+    const imagePath = imagesPath[i];    
+
+    // inserisco nella stringa vuota il codice html del singolo percorso all'atributo src 
+    imagesAdd += `<img src="${imagePath}">`;
+}
+// si prende la stringa popolata e si stampa nel DOM il codice con l'immagine
+document.querySelector('.carousel-container').innerHTML += imagesAdd;
+
+// definisco una variabile, e si assegna l'index 0, impostando la prima immagine come attiva 
+let currentImage = [0];     
+// seleziono tutte le immagini attraverso il querySelectorAll che pesca i tag di tutte le immagini presenti nel container
 const images = document.querySelectorAll('.carousel-container img');
-
-// do la classe active alla prima immagine dell'array
+// do la classe active (display: block) alla prima immagine dell'array, assegnando display: non a tute le altre
 images[currentImage].classList.add('active');
 
-
+// BOTTON UP 
 document.querySelector('#nextBtn').addEventListener('click', function() {
     console.log('avanti');
 
+    // 
     if (currentImage < images.length-1) {
-    images[currentImage].classList.remove('active');
-    currentImage++;
-    images[currentImage].classList.add('active');
+        images[currentImage].classList.remove('active');
+        currentImage++;
+        images[currentImage].classList.add('active');
+    } else if (currentImage == images.length-1) {
+        images[currentImage].classList.remove('active');
+        currentImage = [0];
+        images[currentImage].classList.add('active');
     }
 
 });
@@ -25,6 +52,10 @@ document.querySelector('#prevBtn').addEventListener('click', function() {
     if (currentImage > 0) {
         images[currentImage].classList.remove('active');
         currentImage--;
+        images[currentImage].classList.add('active');
+    } else if (currentImage == 0) {
+        images[currentImage].classList.remove('active');
+        currentImage = images.length-1;
         images[currentImage].classList.add('active');
     }
 });
